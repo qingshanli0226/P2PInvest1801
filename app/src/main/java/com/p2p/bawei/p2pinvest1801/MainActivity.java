18f2dfc471a866fd60dbae477cbaab87e2ac0e4a
84wangmou <2247184831@qq.com>
2020-08-11 14:03:36 +0800

    王某上传

--- app/src/main/java/com/p2p/bawei/p2pinvest1801/MainActivity.java
+++ app/src/main/java/com/p2p/bawei/p2pinvest1801/MainActivity.java
@@ -4,7 +4,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity { //王俊豪练习项目
+
+	
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
