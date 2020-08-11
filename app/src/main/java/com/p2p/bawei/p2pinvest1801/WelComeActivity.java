ad8b5d199eef054fe43784461817cc638ee4a1e1
RenHaiSuSu <1691593808@qq.com>
2020-08-11 15:37:58 +0800

    欢迎页面

---
+++ app/src/main/java/com/p2p/bawei/p2pinvest1801/WelComeActivity.java
@@ -0,0 +1,14 @@
+package com.p2p.bawei.p2pinvest1801;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WelComeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wel_come);
+
+    }
+}
