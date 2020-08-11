6f794eec0d2f305c679d1f7f2348ee3e89e5b964
sunhaowen <1739043011@qq.com>
2020-08-11 20:34:21 +0800

    欢迎页面

--- app/src/main/java/com/p2p/bawei/p2pinvest1801/mvp/view/activity/MainActivity.java
+++ app/src/main/java/com/p2p/bawei/p2pinvest1801/mvp/view/activity/MainActivity.java
@@ -1,16 +1,23 @@
 package com.p2p.bawei.p2pinvest1801.mvp.view.activity;
 
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.RadioGroup;
 
 import com.example.lib_core.mvp.view.BaseActivity;
 import com.p2p.bawei.p2pinvest1801.R;
 
 public class MainActivity extends BaseActivity {
-
+    private ViewPager mainViewVp;
+    private RadioGroup radio;
     @Override
     public void initView() {
+
+
+        mainViewVp = (ViewPager) findViewById(R.id.main_view_vp);
+        radio = (RadioGroup) findViewById(R.id.radio);
 
 
 
