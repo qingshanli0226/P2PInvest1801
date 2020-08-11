48ba992cf50e06e20ea93a573bd311eaab65800d
ChangXuan <1348972190@qq.com>
2020-08-11 16:35:09 +0800

    欢迎

--- app/src/main/java/com/p2p/bawei/p2pinvest1801/MainActivity.java
+++ app/src/main/java/com/p2p/bawei/p2pinvest1801/MainActivity.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -13,5 +12,8 @@ public class MainActivity extends AppCompatActivity {
 
         Log.d("LQS", "1801学习git");
         Log.d("LQS", "常轩学习git");
+
+
+
     }
 }
