3b515ca9553ff9819bd85a6b1fffd9559de444c8
liuxuan00 <33903479@qq.com>
2020-08-12 19:27:26 +0800

    appUI框架

--- app/src/main/java/com/p2p/bawei/p2pinvest1801/MainActivity.java
+++ app/src/main/java/com/p2p/bawei/p2pinvest1801/MainActivity.java
@@ -1,17 +1,73 @@
 package com.p2p.bawei.p2pinvest1801;
 
 import android.os.Bundle;
-import android.util.Log;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import com.next.easynavigation.view.EasyNavigationBar;
+import com.p2p.bawei.p2pinvest1801.fragment.HomeFragment;
+import com.p2p.bawei.p2pinvest1801.fragment.InvestFragment;
+import com.p2p.bawei.p2pinvest1801.fragment.MineFragment;
+import com.p2p.bawei.p2pinvest1801.fragment.MoreFragment;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-
+    private EasyNavigationBar easyBar;
+    private String[] titleItems = {"首页", "投资", "我的资产", "更多"};
+    private int[] normalIconItems = {R.drawable.bottom01, R.drawable.bottom03, R.drawable.bottom05, R.drawable.bottom07};
+    private int[] selectIconItems = {R.drawable.bottom02, R.drawable.bottom04, R.drawable.bottom06, R.drawable.bottom08};
+    private List<Fragment> fragmentList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        easyBar = findViewById(R.id.easyBar);
 
-        Log.d("LQS", "liuxuan学习git");
+        fragmentList = new ArrayList<>();
+        fragmentList.add(new HomeFragment());
+        fragmentList.add(new InvestFragment());
+        fragmentList.add(new MineFragment());
+        fragmentList.add(new MoreFragment());
+
+        easyBar.titleItems(titleItems)
+                .normalIconItems(normalIconItems)
+                .selectIconItems(selectIconItems)
+                .fragmentList(fragmentList)
+                .canScroll(true)//侧滑切换
+//                .centerImageRes(R.mipmap.ic_launcher_round) //最中间图片
+                .fragmentManager(getSupportFragmentManager())
+                .mode(EasyNavigationBar.NavigationMode.MODE_NORMAL)
+                .setOnTabLoadListener(new EasyNavigationBar.OnTabLoadListener() { //tab加载完毕回调
+                    @Override
+                    public void onTabLoadCompleteEvent() {
+                        easyBar.setMsgPointCount(0,9);
+                        easyBar.setMsgPointCount(1,314);
+                        easyBar.setMsgPointCount(2,42);
+                        easyBar.setHintPoint(3, true);
+                    }
+                })
+                .setOnTabClickListener(new EasyNavigationBar.OnTabClickListener() {
+                    @Override
+                    public boolean onTabSelectEvent(View view, int position) {
+                        easyBar.clearMsgPoint(position); //消息量消失
+                        easyBar.clearHintPoint(position);//红点消失
+                        return false;
+                    }
+
+                    @Override
+                    public boolean onTabReSelectEvent(View view, int position) {
+                        return false;
+                    }
+                })
+                .build();
+
+
+
+
+
     }
 }
---
+++ app/src/main/java/com/p2p/bawei/p2pinvest1801/fragment/InvestFragment.java
@@ -0,0 +1,26 @@
+package com.p2p.bawei.p2pinvest1801.fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.p2p.bawei.p2pinvest1801.R;
+
+public class InvestFragment extends Fragment {
+
+    public InvestFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_invest, container, false);
+    }
+
+}
