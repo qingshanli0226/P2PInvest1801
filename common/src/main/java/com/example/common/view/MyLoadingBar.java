2ce8928a766973673350f588e6fddd4e947c2c07
zhaoxuhui <1354238272@qq.com>
2020-08-12 09:59:11 +0800

    添加了自定义loadingBar

---
+++ common/src/main/java/com/example/common/view/MyLoadingBar.java
@@ -0,0 +1,29 @@
+package com.example.common.view;
+
+import android.content.Context;
+import android.graphics.drawable.AnimationDrawable;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+import com.example.common.R;
+
+public class MyLoadingBar extends LinearLayout {
+
+    private ImageView iv;
+
+    public MyLoadingBar(Context context) {
+        super(context);
+    }
+
+    public MyLoadingBar(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        LayoutInflater.from(context).inflate(R.layout.loading_view, this);
+
+        iv = findViewById(R.id.my_loadingbar);
+        AnimationDrawable drawable = (AnimationDrawable) iv.getDrawable();
+        drawable.start();
+
+    }
+}
