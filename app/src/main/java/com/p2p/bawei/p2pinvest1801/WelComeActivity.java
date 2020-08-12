8a41d3a4aeb831de096428d27ce11075e139d9c4
jialeliu <1005400916@qq.com>
2020-08-12 20:05:38 +0800

    提交多线程执行任务

--- app/src/main/java/com/p2p/bawei/p2pinvest1801/WelComeActivity.java
+++ app/src/main/java/com/p2p/bawei/p2pinvest1801/WelComeActivity.java
@@ -1,16 +1,21 @@
 package com.p2p.bawei.p2pinvest1801;
 
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Handler;
 import android.os.Message;
 import android.support.annotation.NonNull;
+import android.support.v7.app.AlertDialog;
 import android.view.View;
 import android.view.animation.AccelerateInterpolator;
 import android.view.animation.AlphaAnimation;
 import android.view.animation.Animation;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.p2p.bawei.p2pinvest1801.http.NetType;
 import com.p2p.bawei.p2pinvest1801.mvp.view.BaseActivity;
 
 import java.util.Timer;
@@ -24,11 +29,6 @@ public class WelComeActivity extends BaseActivity {
         @Override
         public void handleMessage(@NonNull Message msg) {
             super.handleMessage(msg);
-            if (msg.what == 0){
-                Intent intent = new Intent(WelComeActivity.this, Main2Activity.class);
-                startActivity(intent);
-
-            }
             if (msg.what == 101){
                 tvIdWel.setText(theTime+"");
                 theTime--;
@@ -76,7 +76,7 @@ public class WelComeActivity extends BaseActivity {
 
             @Override
             public void onAnimationEnd(Animation animation) {
-                handler.sendEmptyMessage(0);
+                isUpApp();
             }
 
             @Override
@@ -97,4 +97,51 @@ public class WelComeActivity extends BaseActivity {
     public int banLayout() {
         return R.layout.activity_main;
     }
+
+    private void isUpApp() {
+        new AlertDialog.Builder(this)
+                .setTitle("是否更新")
+                .setPositiveButton("取消", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Intent intent = new Intent(WelComeActivity.this, Main2Activity.class);
+                        startActivity(intent);
+                    }
+                })
+                .setNegativeButton("确定", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        if (!NetType.isNetContent(WelComeActivity.this)) {
+                            Toast.makeText(WelComeActivity.this, "请检查网络状态", Toast.LENGTH_SHORT).show();
+                            return;
+                        }
+                        upApp();
+                    }
+
+                }).show();
+    }
+    private void upApp() {
+        final ProgressDialog progressDialog = new ProgressDialog(WelComeActivity.this);
+        progressDialog.setMax(100);
+
+        progressDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+        progressDialog.show();
+
+        final Timer timer = new Timer();
+        timer.schedule(new TimerTask() {
+            private int index = 0;
+
+            @Override
+            public void run() {
+                progressDialog.setProgress(index++);
+                if (progressDialog.getProgress() == 100) {
+                    timer.cancel();
+                    finish();
+                    Intent intent = new Intent(WelComeActivity.this, Main2Activity.class);
+                    startActivity(intent);
+                }
+            }
+        }, 0, 100);
+    }
+
 }
