4e0640c49e04d5a580ce2ed0784df7ddd7254a5f
jialeliu <1005400916@qq.com>
2020-08-19 20:54:41 +0800

    全部布局

--- app/src/main/java/com/p2p/bawei/p2pinvest1801/fragmentfour/FragmentFour.java
+++ app/src/main/java/com/p2p/bawei/p2pinvest1801/fragmentfour/FragmentFour.java
@@ -1,9 +1,15 @@
 package com.p2p.bawei.p2pinvest1801.fragmentfour;
 
+import android.view.View;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
 import com.p2p.bawei.p2pinvest1801.R;
 import com.p2p.bawei.p2pinvest1801.mvp.view.BaseFragment;
 
 public class FragmentFour extends BaseFragment {
+    private boolean isClicked = false;
+    private ImageView moreToggle;
 
     @Override
     public int banLayout() {
@@ -12,12 +18,23 @@ public class FragmentFour extends BaseFragment {
 
     @Override
     public void initView() {
-
+        moreToggle = (ImageView) findViewById(R.id.more_toggle);
     }
 
     @Override
     public void initData() {
-
+        moreToggle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (isClicked){
+                    Glide.with(getContext()).load(R.drawable.toggle_off).into(moreToggle);
+                    isClicked = false;
+                }else {
+                    Glide.with(getContext()).load(R.drawable.toggle_on).into(moreToggle);
+                    isClicked =true;
+                }
+            }
+        });
     }
 
     @Override
